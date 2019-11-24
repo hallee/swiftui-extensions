022bb58fe57e10304c9766ec710d3c60f84bb89f
Hal Lee <hal@lee.me>
2019-11-24 16:15:15 -0500

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "SwiftUIExtensions",
+    platforms: [
+        .iOS(.v13), .macOS(.v10_15), .tvOS(.v13), .watchOS(.v6)
+    ],
+    products: [
+    	.library(name: "SwiftUIExtensions", targets: ["SwiftUIExtensions"])
+    ],
+    targets: [
+        .target(name: "SwiftUIExtensions"),
+        .testTarget(name: "Tests", dependencies: ["SwiftUIExtensions"], path: "Tests/SwiftUIExtensions")
+    ]
+)
---
+++ Tests/SwiftUIExtensions/Tests.swift
@@ -0,0 +1,7 @@
+import XCTest
+import SwiftUI
+@testable import SwiftUIExtensions
+
+final class SwiftUIExtensionsTests: XCTestCase {
+
+}
